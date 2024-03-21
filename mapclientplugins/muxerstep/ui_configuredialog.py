960505ffb652be0e91e82a1a05a116447de0612e
Hugh Sorby <h.sorby@auckland.ac.nz>
2024-03-21 23:08:37 +1300

    Make the mux type an editable combobox.

--- mapclientplugins/muxerstep/ui_configuredialog.py
+++ mapclientplugins/muxerstep/ui_configuredialog.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'configuredialog.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.4.1
+## Created by: Qt User Interface Compiler version 6.5.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -15,9 +15,10 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
-    QFormLayout, QGridLayout, QGroupBox, QLabel,
-    QLineEdit, QSizePolicy, QSpinBox, QWidget)
+from PySide6.QtWidgets import (QAbstractButton, QApplication, QComboBox, QDialog,
+    QDialogButtonBox, QFormLayout, QGridLayout, QGroupBox,
+    QLabel, QLineEdit, QSizePolicy, QSpinBox,
+    QWidget)
 
 class Ui_ConfigureDialog(object):
     def setupUi(self, ConfigureDialog):
@@ -30,37 +31,40 @@ class Ui_ConfigureDialog(object):
         self.configGroupBox.setObjectName(u"configGroupBox")
         self.formLayout = QFormLayout(self.configGroupBox)
         self.formLayout.setObjectName(u"formLayout")
-        self.label0 = QLabel(self.configGroupBox)
-        self.label0.setObjectName(u"label0")
+        self.labelIdentifier = QLabel(self.configGroupBox)
+        self.labelIdentifier.setObjectName(u"labelIdentifier")
 
-        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label0)
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.labelIdentifier)
 
         self.lineEditIdentifier = QLineEdit(self.configGroupBox)
         self.lineEditIdentifier.setObjectName(u"lineEditIdentifier")
 
         self.formLayout.setWidget(0, QFormLayout.FieldRole, self.lineEditIdentifier)
 
-        self.label = QLabel(self.configGroupBox)
-        self.label.setObjectName(u"label")
+        self.labelMuxType = QLabel(self.configGroupBox)
+        self.labelMuxType.setObjectName(u"labelMuxType")
 
-        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.label)
+        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.labelMuxType)
 
-        self.lineEditPortType = QLineEdit(self.configGroupBox)
-        self.lineEditPortType.setObjectName(u"lineEditPortType")
+        self.labelNumberOfInputs = QLabel(self.configGroupBox)
+        self.labelNumberOfInputs.setObjectName(u"labelNumberOfInputs")
 
-        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.lineEditPortType)
-
-        self.label_2 = QLabel(self.configGroupBox)
-        self.label_2.setObjectName(u"label_2")
-
-        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.label_2)
+        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.labelNumberOfInputs)
 
         self.spinBoxNumberOfInputs = QSpinBox(self.configGroupBox)
         self.spinBoxNumberOfInputs.setObjectName(u"spinBoxNumberOfInputs")
         self.spinBoxNumberOfInputs.setMinimumSize(QSize(75, 0))
         self.spinBoxNumberOfInputs.setMinimum(1)
+        self.spinBoxNumberOfInputs.setValue(2)
 
         self.formLayout.setWidget(2, QFormLayout.FieldRole, self.spinBoxNumberOfInputs)
+
+        self.comboBoxMuxType = QComboBox(self.configGroupBox)
+        self.comboBoxMuxType.addItem("")
+        self.comboBoxMuxType.setObjectName(u"comboBoxMuxType")
+        self.comboBoxMuxType.setEditable(True)
+
+        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.comboBoxMuxType)
 
 
         self.gridLayout.addWidget(self.configGroupBox, 0, 0, 1, 1)
@@ -83,9 +87,11 @@ class Ui_ConfigureDialog(object):
     def retranslateUi(self, ConfigureDialog):
         ConfigureDialog.setWindowTitle(QCoreApplication.translate("ConfigureDialog", u"Configure Muxer", None))
         self.configGroupBox.setTitle("")
-        self.label0.setText(QCoreApplication.translate("ConfigureDialog", u"Identifier:  ", None))
+        self.labelIdentifier.setText(QCoreApplication.translate("ConfigureDialog", u"Identifier:  ", None))
         self.lineEditIdentifier.setText(QCoreApplication.translate("ConfigureDialog", u"Muxer", None))
-        self.label.setText(QCoreApplication.translate("ConfigureDialog", u"Mux type:", None))
-        self.label_2.setText(QCoreApplication.translate("ConfigureDialog", u"Number of inputs:", None))
+        self.labelMuxType.setText(QCoreApplication.translate("ConfigureDialog", u"Mux type:", None))
+        self.labelNumberOfInputs.setText(QCoreApplication.translate("ConfigureDialog", u"Number of inputs:", None))
+        self.comboBoxMuxType.setItemText(0, QCoreApplication.translate("ConfigureDialog", u"http://physiomeproject.org/workflow/1.0/rdf-schema#file_location", None))
+
     # retranslateUi
 
